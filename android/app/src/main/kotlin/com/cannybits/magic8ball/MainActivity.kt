3d56a910492e8239b78457aa93c699592a4ba0f4
nyandago <paulmutator@gmail.com>
2022-01-11 16:04:28 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/cannybits/magic8ball/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.cannybits.magic8ball
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text('Magic8Ball'),
+        backgroundColor: Colors.blue,
+      ),
+
+      body: MyApp(),
+    ),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+   
+    );
+  }
+}
+
